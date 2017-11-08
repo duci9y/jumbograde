f351ccbad795cdd8ee6d08135966456df8cb859a
StevieSong <steven.song98@gmail.com>
2017-11-07 21:42:14 -0500

    initial view done

--- jumbograde/jinja2.py
+++ jumbograde/jinja2.py
@@ -1,13 +1,14 @@
 from jinja2 import Environment
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 def environment(**options):
     env = Environment(**options)
-    # env.globals.update({
-    #     'static': staticfiles_storage.url,
+    env.globals.update({
+        'static': staticfiles_storage.url,
     #     'url': reverse,
     #     'compile': compile_static,
     #     'serialize_item': serialize_item,
     #     'markdownify': markdownify,
     #     'visible_pages': visible_pages,
-    # })
+    })
     return env
