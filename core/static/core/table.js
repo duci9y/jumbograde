f53eb52a1b43394a21e69233afa98a16410318e2
Jennifer Leung <jenniferleung9@gmail.com>
2017-11-10 19:40:49 -0500

    add back to courses button

--- core/static/core/table.js
+++ core/static/core/table.js
@@ -2,3 +2,7 @@ $('.table > tbody > tr').click(function() {
     var id = $(this).find('td:eq(0)').text();
     location.href = id;
 });
+
+$('#to_courses' ).click(function() {
+    location.href = '../';
+});
