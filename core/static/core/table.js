5febe7f2deb4ed34753b09d20a7c2c884a5d07b1
Jennifer Leung <jenniferleung9@gmail.com>
2017-11-10 20:35:26 -0500

    use jinja templating to populate tables

--- core/static/core/table.js
+++ core/static/core/table.js
@@ -1,8 +1,15 @@
+/* JQuery */
+
+/* go to corresponding assignment scorecard */
 $('.table > tbody > tr').click(function() {
     var id = $(this).find('td:eq(0)').text();
     location.href = id;
 });
 
-$('#to_courses' ).click(function() {
+
+/* JavaScript */
+
+/* go back to previous page */
+function go_back() {
     location.href = '../';
-});
+}
