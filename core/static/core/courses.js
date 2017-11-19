a264552bbd5b67581bd6d3bffc4fb06f8168de0e
Jennifer Leung <jenniferleung9@gmail.com>
2017-11-19 16:46:30 -0500

    update to follow bootstrap guidelines

--- core/static/core/courses.js
+++ core/static/core/courses.js
@@ -1,5 +1,11 @@
-$("button").click(function() {
+// redirect to grades for specified course
+$(".course").click(function() {
     var course = $(this).text();
     location.href = "../" + course + "/view_grades";
 });
 
+// redirec to login page
+$(".logout").click(function() {
+    var course = $(this).text();
+    location.href = "../login";
+});
--- core/static/core/table.js
+++ core/static/core/table.js
@@ -1,15 +1,11 @@
-/*************************     JQuery     **************************/
-
-/* go to corresponding assignment scorecard */
+// redirect to corresponding assignment scorecard 
 $('.table > tbody > tr').click(function() {
     var id = $(this).find('td:eq(0)').text();
     location.href = id;
 });
 
 
-/*************************   JavaScript   **************************/
-
-/* go back to previous page */
+// redirect back to courses page 
 function go_back_to_courses() {
     location.href = "../../courses";
 }
