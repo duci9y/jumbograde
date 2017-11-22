d686f7e0a38444fb0445e593ca225200500a4e87
Deepanshu Utkarsh <deepanshu.utkarsh@tufts.edu>
2017-11-21 22:47:32 -0500

    Integrate login template with login flow

--- jumbograde/jinja2.py
+++ jumbograde/jinja2.py
@@ -1,11 +1,12 @@
 from jinja2 import Environment
 from django.contrib.staticfiles.storage import staticfiles_storage
+from django.core.urlresolvers import reverse
 
 def environment(**options):
     env = Environment(**options)
     env.globals.update({
         'static': staticfiles_storage.url,
-    #     'url': reverse,
+        'url': reverse,
     #     'compile': compile_static,
     #     'serialize_item': serialize_item,
     #     'markdownify': markdownify,
