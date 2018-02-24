26786d8833016b2f25dfef9f81d2eeac1a2e6df6
Leland Wu <leland.wu4@gmail.com>
2018-02-24 16:14:07 -0500

    implement basic finished scorecard view

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
-from core.views import AssignmentStudentView
+from core.views import AssignmentStudentView, ScorecardStudentView
 
 urlpatterns = [
     url('', include('django.contrib.auth.urls')),
     url(r'^test_endpoint/$', TemplateView.as_view(template_name='core/test.html.j2')),
-    url(r'^courses/$', TemplateView.as_view(template_name='core/courses.html.j2'
-    )),
-    url(r'view_grades/$', AssignmentStudentView.as_view())
+    url(r'^courses/$', TemplateView.as_view(template_name='core/courses.html.j2')),
+    url(r'view_grades/$', AssignmentStudentView.as_view()),
+    url(r'^view_scorecard/', ScorecardStudentView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -12,6 +12,56 @@ class AssignmentStudentView(View):
         }
         return render(request, 'core/table.html.j2', context) 
 
+class ScorecardStudentView(View):
+     def get(self, request):
+        scorecard = [
+                {
+                        "group_name": "group1",
+                        "sections": [
+                            {
+                                "section_name": "Part 1",
+                                "items": [
+                                    {
+                                        "description": "Does this",
+                                        "points_max": 4,
+                                        "points_received": 4
+                                    }
+                                ],
+                                "comments": "good job"
+                            },
+                            {
+                                "section_name": "Part 2",
+                                "items": [
+                                    {
+                                        "description": "does that",
+                                        "points_max": 10,
+                                        "points_received": 5
+                                    }
+                                ],
+                                "comments": "you missed the part where you did that thing"
+                            },
+                             {
+                                "section_name": "Part 3",
+                                "items": [
+                                    {
+                                        "description": "does everything",
+                                        "points_max": 6,
+                                        "points_received": 6
+                                    }
+                                ],
+                                "comments": "wow!"
+                            }
+
+                        ],
+                        
+                },
+        ]
+        context = {
+            'scorecard' : scorecard,
+        }
+        return render(request, 'core/view_scorecard.html.j2', context) 
+
+
 
 
 
