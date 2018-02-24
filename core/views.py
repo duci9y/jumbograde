de802738350df252b35bda033daa854f5b982ef2
Jennifer Leung <jenniferleung9@gmail.com>
2018-02-24 17:18:31 -0500

    write views for selecting course and assignment

--- core/views.py
+++ core/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.list import ListView
+from core.models import Course, Assignment
 
-from .models import Scorecard
-from .models import Assignment
+# Using class based views
+class CourseList(ListView):
+    template_name = 'core/courses.html.j2'
 
-# Create your views here.
-class GradeView(View):
-        def get(self, request):
-                assignment_list = Assignment.objects.order_by('display_name')
-                context = {
-                        'assignment_list' : assignment_list,
-                }
-                return render(request, 'core/grade.html.j2', context)
+    def get_queryset(self):
+        return Course.objects.order_by('course_number')
+
+class AssignmentList(ListView):
+    template_name = 'core/assignments.html.j2'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['course_name'] = self.kwargs['course_number']
+        return context
+
+    def get_queryset(self):
+        return Assignment.objects.order_by('display_name')
