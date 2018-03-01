e9b21ce45bce253c500659c3337f5fbd1a9c3aaa
Deepanshu Utkarsh <deepanshu.utkarsh@tufts.edu>
2018-03-01 18:59:29 -0500

    add comma to end of list

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='core/login.html.j2'), name='login'),
     url('', include('django.contrib.auth.urls')),
-    url(r'^test_endpoint/$', TemplateView.as_view(template_name='core/test.html.j2'))
+    url(r'^test_endpoint/$', TemplateView.as_view(template_name='core/test.html.j2')),
 ]
