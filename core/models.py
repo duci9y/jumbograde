d258e237c1f2cc7b9d03c5799b5dd62e57ffdb9c
Deepanshu Utkarsh <deepanshu.utkarsh@tufts.edu>
2018-03-14 16:06:25 -0400

    Revert "Revert "Rename ScorecardTemplate""
    This reverts commit e2019bb8762d70d07952661ecd16b8a5ddd8986f.

--- core/models.py
+++ core/models.py
@@ -55,9 +55,9 @@ class Assignment(models.Model):
         return self.display_name
 
 
-class ScorecardFormat(models.Model):
+class ScorecardTemplate(models.Model):
     # TODO: Think more
-    assignment = models.OneToOneField(Assignment, related_name='scorecard_format', on_delete=models.CASCADE)
+    assignment = models.OneToOneField(Assignment, related_name='scorecard_template', on_delete=models.CASCADE)
     data = JSONField()
 
 
