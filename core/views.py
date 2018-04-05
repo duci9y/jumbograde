ac9bf2aa2793190fd4e0975fe03111157eb5efda
Jennifer Leung <jenniferleung9@gmail.com>
2018-04-05 19:36:36 -0400

    initial html for scorecard, fix hardcoded urls

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.views.generic import DetailView
 from django.views.generic.list import ListView
 from core.models import Course, Assignment
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -10,7 +11,6 @@ class CourseList(LoginRequiredMixin, ListView):
     template_name = 'core/courses.html.j2'
     login_url = '/login/'
     redirect_field_name = 'redirect_to'
-
 
     def get_queryset(self):
         user_groups = self.request.user.groups.values_list('id', flat=True)
@@ -34,4 +34,63 @@ class AssignmentList(LoginRequiredMixin, ListView):
 
         if course.grader_posix_group not in self.request.user.groups.all():
             return redirect(reverse('course_list'))
+
         return super(AssignmentList, self).dispatch(request, *args, **kwargs)
+
+
+class ScorecardView(LoginRequiredMixin, DetailView):
+    login_url = '/login/'
+    redirect_filed_name = 'redirect_to'
+
+    def get(self, request):
+        scorecard = [
+            {
+                "group_name": "name",
+                "sections": [
+                    {
+                        "section_name": "patient",
+                        "items": [
+                            {
+                                "description": "calc prior",
+                                "points": 4
+                            },
+                            {
+                                "description": "comp op blah blah here are words to test things on multiple lines hello hello",
+                                "points": 8
+                            },
+                            {
+                                "description": "something",
+                                "points": 12
+                            }
+                        ]
+                    },
+                    {
+                        "section_name": "minheap",
+                        "items": [
+                            {
+                                "description": "heapify",
+                                "points": 2
+                            }
+                        ]
+                    },
+                    {
+                        "section_name": "maxheap",
+                        "items": [
+                            {
+                                "description": "extract",
+                                "points": 6
+                            },
+                            {
+                                "description": "insert",
+                                "points": 4
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+
+        context = {
+            'scorecard' : scorecard,
+        }
+        return render(request, 'core/scorecard.html.j2', context)
